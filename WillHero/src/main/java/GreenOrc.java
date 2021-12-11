2a2546e3de7b222496457a124a7db9b21a3de643
Deepam Sarmah <deepam20050@iiitd.ac.in>
2021-12-11 12:04:14 +0530

    12:04

--- WillHero/src/main/java/GreenOrc.java
+++ WillHero/src/main/java/GreenOrc.java
@@ -5,7 +5,8 @@ public class GreenOrc extends Orc {
   private String imagePath;
   private ImageView imageView;
   private double size;
-  private double jumpSpeed;
+  private double jumpSpeed1;
+  private double jumpSpeed2;
   private double gravity;
 
   public GreenOrc (double x, double y, double _size) {
@@ -14,7 +15,8 @@ public class GreenOrc extends Orc {
      */
     super(x, y, 1, 1, 1);
     size = _size;
-    jumpSpeed = 8;
+    jumpSpeed1 = 8;
+    jumpSpeed2 = 3;
     gravity = 0.25;
 
     imagePath = "file:assets/GreenOrcSprite.png";
@@ -26,6 +28,24 @@ public class GreenOrc extends Orc {
     imageView.setFitWidth(size);
     imageView.setPreserveRatio(true);
     imageView.setSmooth(true);
+  }
+  public void jump_up () {
+    this.setVelocity(getVelocity().getX(), -jumpSpeed2);
+    this.setVelocity(getVelocity().getX(), -jumpSpeed2);
+    this.setVelocity(getVelocity().getX(), -jumpSpeed1);
+  }
+  public void if_lands (Island island) {
+    boolean hasLanded = false;
+    double xdist = this.getPosition().getX() - island.getPosition().getX();
+    if (xdist >= -size && xdist <= island.getLength()) {
+      double ydist = this.getPosition().getY() - island.getPosition().getY();
+      if (ydist >= -size && ydist < 0) {
+        hasLanded = true;
+      }
+    }
+    if (hasLanded) {
+      jump_up();
+    }
   }
   public ImageView getImageView() {
     return imageView;
