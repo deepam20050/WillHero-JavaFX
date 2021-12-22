86bffb6c4c7e1baa8c8b3360b613972b543c5f9f
Deepam Sarmah <deepam20050@iiitd.ac.in>
2021-12-22 17:48:59 +0530

    latest

--- WillHero/src/main/java/GreenOrc.java
+++ WillHero/src/main/java/GreenOrc.java
@@ -1,26 +1,11 @@
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class GreenOrc extends Orc {
-    private String imagePath;
-    private double size;
-    private double jumpSpeed1;
-    private double jumpSpeed2;
-    private double gravity;
-
-    private int jump_counter;
-
     public GreenOrc (double x, double y, double _size) {
-        /* For now jump_speed, size, hits_required initialized to 1
-         * Can change if required
-         */
-        super(x, y, 1, _size, 1);
+        super(x, y, _size, 1);
         size = _size;
-        jumpSpeed1 = 8;
-        jumpSpeed2 = 3;
-        gravity = 0.25;
         prize = 1;
-
+        pushVelocity = 20;
         imagePath = "file:assets/GreenOrcSprite.png";
         this.setImage(new Image(imagePath));
         double w = getImageView().getImage().getWidth();
@@ -30,53 +15,6 @@ public class GreenOrc extends Orc {
         getImageView().setFitWidth(size);
         getImageView().setPreserveRatio(true);
         getImageView().setSmooth(true);
-
         jump_counter = 0;
-    }
-
-    @Override
-    public void move_down()
-    {
-        this.setVelocity(getVelocity().getX(), getVelocity().getY() + gravity);
-    }
-
-    @Override
-    public void updatePosition(double cameraPosition)
-    {
-        this.setPosition(getPosition().getX() + getVelocity().getX(), getPosition().getY() + getVelocity().getY());
-
-        ImageView imageView = getImageView();
-
-        double w = imageView.getImage().getWidth();
-        double h = imageView.getImage().getHeight();
-        imageView.setX(getPosition().getX() - cameraPosition);
-        imageView.setY(getPosition().getY() - (h-w)*(size/w));
-    }
-
-    public void jump_up ()
-    {
-        if(jump_counter == 0) {
-            this.setVelocity(getVelocity().getX(), -jumpSpeed1);
-        }
-        else {
-            this.setVelocity(getVelocity().getX(), -jumpSpeed2);
-        }
-        jump_counter = (jump_counter + 1) % 3;
-    }
-
-
-    public void if_lands (Island island) {
-        boolean hasLanded = false;
-        double xdist = this.getPosition().getX() - island.getPosition().getX();
-        if (xdist >= -size && xdist <= island.getLength()) {
-            double ydist = this.getPosition().getY() - island.getPosition().getY();
-            if (ydist >= -size && ydist < 0) {
-                hasLanded = true;
-            }
-        }
-        if (hasLanded) {
-            this.setVelocity(0, 0);
-            jump_up();
-        }
     }
 }
--- WillHero/src/main/java/Orc.java
+++ WillHero/src/main/java/Orc.java
@@ -1,25 +1,61 @@
 import javafx.scene.image.ImageView;
 
 public abstract class Orc extends GameObject {
-    private double jump_speed;
-    private double size;
+    protected double size;
     protected int prize;
     protected int dead;
     protected int hits_required;
+    protected double pushVelocity;
+    protected String imagePath;
+    protected double jumpSpeed1;
+    protected double jumpSpeed2;
+    protected int jump_counter;
+    private final double gravity;
     private final int fallBoundary = 600;
 
-    public Orc (double x, double y, double _jump_speed, double _size, int _hits_required) {
+    public Orc (double x, double y, double _size, int _hits_required) {
         super(new Vector2D (x, y), new Vector2D (0, 0));
-        jump_speed = _jump_speed;
         size = _size;
         hits_required = _hits_required;
+        jumpSpeed1 = 8;
+        jumpSpeed2 = 3;
+        gravity = 0.25;
         dead = 1;
     }
 
-    public abstract void move_down();
-    public abstract void updatePosition(double cameraPosition);
-    public abstract void jump_up ();
-    public abstract void if_lands (Island island);
+    public void move_down() {
+        this.setVelocity(getVelocity().getX(), getVelocity().getY() + gravity);
+    }
+    public void updatePosition(double cameraPosition) {
+        this.setPosition(getPosition().getX() + getVelocity().getX(), getPosition().getY() + getVelocity().getY());
+        ImageView imageView = getImageView();
+        double w = imageView.getImage().getWidth();
+        double h = imageView.getImage().getHeight();
+        imageView.setX(getPosition().getX() - cameraPosition);
+        imageView.setY(getPosition().getY() - (h-w)*(size/w));
+    }
+    public void jump_up ()  {
+        if(jump_counter == 0) {
+            this.setVelocity(getVelocity().getX(), -jumpSpeed1);
+        } else {
+            this.setVelocity(getVelocity().getX(), -jumpSpeed2);
+        }
+        jump_counter = (jump_counter + 1) % 3;
+    }
+    public void if_lands (Island island) {
+        boolean hasLanded = false;
+        double xdist = this.getPosition().getX() - island.getPosition().getX();
+        if (xdist >= -size && xdist <= island.getLength()) {
+            double ydist = this.getPosition().getY() - island.getPosition().getY();
+            if (ydist >= -size && ydist < 0) {
+                hasLanded = true;
+            }
+        }
+        if (hasLanded) {
+            this.setVelocity(0, 0);
+            jump_up();
+        }
+    }
     public void give_coin (Hero hero) {
         if (dead == 1) return;
         if (dead == 0) {
@@ -45,24 +81,6 @@ public abstract class Orc extends GameObject {
     public double get_size () {
         return size;
     }
-//    @Override
-//    public void if_collides (Hero hero)
-//    {
-//        Vector2D hero_pos = hero.getPosition();
-//        Vector2D orc_pos = this.getPosition();
-//        double hero_size = hero.getSize();
-//        double orc_size = this.size;
-//        double dx = orc_pos.getX() - hero_pos.getX();
-//        double dy = orc_pos.getY() - hero_pos.getY();
-//        if (dx < 0) return;
-//        if (dx > hero_size + orc_size) return;
-//        if (dy > 0 && dy <= orc_size && dx <= hero_size) {
-//            hero.lose_game();
-//        }
-//        if (dx <= hero_size) {
-//            this.setVelocity(30, 0);
-//        }
-//    }
     @Override
     public void if_collides(Hero hero)
     {
@@ -95,10 +113,9 @@ public abstract class Orc extends GameObject {
         else if(isPushed)
         {
             hero.getVelocity().setX(0);
-            this.setVelocity(20, 0);
+            this.setVelocity(pushVelocity, 0);
         }
     }
-
     public void if_collides_with_orc(Orc orc)
     {
         boolean colliding = false;
